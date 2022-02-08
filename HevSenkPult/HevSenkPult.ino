033a4fb202406c75d78119bd3cebcb6ea532b6fb
oddskj <51134353+oddskj@users.noreply.github.com>
2022-02-08 21:53:15 +0100

    Begin Analog input test

--- HevSenkPult/HevSenkPult.ino
+++ HevSenkPult/HevSenkPult.ino
@@ -3,9 +3,14 @@ const int nerPin = 0;
 const int oppPin = 1;
 const int buttonNerPin = 3;
 const int buttonOppPin = 2; 
+const int buttonSet1Pin = 16;
+const int buttonSet2Pin = 17;
 
 int buttonNerState = 0;
 int buttonOppState = 0;
+int buttonSet1State = 0;
+int buttonSet2State = 0;
+
 
 void setup() {
   
@@ -15,12 +20,16 @@ void setup() {
   pinMode(oppPin, OUTPUT);
   pinMode(buttonNerPin, INPUT_PULLDOWN);
   pinMode(buttonOppPin, INPUT_PULLDOWN);
+  pinMode(buttonSet1Pin, INPUT_PULLDOWN);
+  pinMode(buttonSet2Pin, INPUT_PULLDOWN);
 }
 
 void loop() {
 
   buttonNerState = digitalRead(buttonNerPin);
   buttonOppState = digitalRead(buttonOppPin);
+  buttonSet1State = digitalRead(buttonSet1Pin);
+  buttonSet2State = digitalRead(buttonSet2Pin);
 
   if (buttonNerState == HIGH && buttonOppState == HIGH) {
       digitalWrite(nerPin, LOW);
@@ -31,6 +40,7 @@ void loop() {
   } else if (buttonNerState == HIGH) {
       digitalWrite(nerPin, HIGH);
       digitalWrite(oppPin, LOW);
+  } else if () {
   } else {
       digitalWrite(nerPin, LOW);
       digitalWrite(oppPin, LOW);
