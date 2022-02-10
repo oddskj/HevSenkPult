e0b5e1076b3c9bf28fe4736983be399b07817ae9
oddskj <51134353+oddskj@users.noreply.github.com>
2022-02-10 21:43:04 +0100

    Changes to be committed: 	modified:   HevSenkPult/HevSenkPult.ino

--- HevSenkPult/HevSenkPult.ino
+++ HevSenkPult/HevSenkPult.ino
@@ -1,15 +1,20 @@
 
+const int preset1 = 900;
+const int preset2 = 100;
+
 const int nerPin = 0;
 const int oppPin = 1;
 const int buttonNerPin = 3;
 const int buttonOppPin = 2; 
 const int buttonSet1Pin = 16;
 const int buttonSet2Pin = 17;
+const int analogPin = A2;
 
 int buttonNerState = 0;
 int buttonOppState = 0;
 int buttonSet1State = 0;
 int buttonSet2State = 0;
+int analogState = 0;
 
 
 void setup() {
@@ -30,6 +35,7 @@ void loop() {
   buttonOppState = digitalRead(buttonOppPin);
   buttonSet1State = digitalRead(buttonSet1Pin);
   buttonSet2State = digitalRead(buttonSet2Pin);
+  analogState = analogRead(analogPin);
 
   if (buttonNerState == HIGH && buttonOppState == HIGH) {
       digitalWrite(nerPin, LOW);
@@ -40,15 +46,33 @@ void loop() {
   } else if (buttonNerState == HIGH) {
       digitalWrite(nerPin, HIGH);
       digitalWrite(oppPin, LOW);
-  } else if () {
+  } else if (buttonSet1Pin == HIGH && analogState != preset1) {
+      if (analogState < preset1) {
+        while (analogState < preset1) {
+          digitalWrite(nerPin, LOW);
+          digitalWrite(oppPin, HIGH);
+        }
+      } else if (analogState > preset1) {
+        while (analogState > preset1) {
+          digitalWrite(oppPin, LOW);
+          digitalWrite(nerPin, HIGH);
+        }
+      }
+  } else if (buttonSet2Pin == HIGH && analogState != preset2) {
+      if (analogState < preset2) {
+        while (analogState < preset2) {
+          digitalWrite(nerPin, LOW);
+          digitalWrite(oppPin, HIGH);
+        }
+      } else if (analogState > preset2) {
+        while (analogState > preset2) {
+          digitalWrite(oppPin, LOW);
+          digitalWrite(nerPin, HIGH);
+        }
+      }
   } else {
       digitalWrite(nerPin, LOW);
       digitalWrite(oppPin, LOW);
   }
 
-  int sensor = analogRead(A2);
-
-  float volt = sensor * (10.0 / 1023.0);
-
-  Serial.println(volt);
 }
